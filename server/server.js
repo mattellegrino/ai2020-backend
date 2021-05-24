bb12298aa09e2d2e3008773cdcc4353898868674
ScovottoDavide <scovottodavide@gmail.com>
2021-05-24 10:29:59 +0200

    Start point

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const { check, validationResult } = require("express-validator"); // validation middleware
 const dao = require('./dao');
 
 const PORT = 3001;
@@ -13,10 +14,7 @@ app.use(express.json());
 app.get('/api/tasks', async (req, res) => {
     try {
         const tasks = await dao.listTasks();
-        if (tasks.error)
-            res.status(404).json(tasks);
-        else
-            res.json(tasks);
+        res.json(tasks);
     }
     catch (err) {
         res.status(500).end();
@@ -52,49 +50,73 @@ app.get('/api/tasks/:id', async (req, res) => {
 })
 
 // Create task
-app.post('/api/tasks', async (req, res) => {
-    const newtask = {
-        description: req.body.description,
-        important: req.body.urgent,
-        private: req.body.private,
-        deadline: req.body.deadline
-    };
-    await console.log(newtask);
-    try {
-        await dao.createTask(newtask);
-        res.status(201).end();
-    }
-    catch (err) {
-        res.status(503).json({ error: `Database error during creation of task ${task.description}` });
-    }
-})
+app.post('/api/tasks',
+    [
+        check("description").isLength({ max: 40 }),
+        check("urgent").isBoolean(),
+        check("private").isBoolean(),
+        check("deadline").isDate({ format: "YYYY-MM-DD", strictMode: true }),
+    ],
+    async (req, res) => {
+        const newtask = {
+            description: req.body.description,
+            important: req.body.urgent,
+            private: req.body.private,
+            deadline: req.body.deadline
+        };
+        try {
+            await dao.createTask(newtask);
+            res.status(201).end();
+        }
+        catch (err) {
+            res.status(503).json({ error: `Database error during creation of task ${task.description}` });
+        }
+    })
 
 // Update task
-app.put('/api/tasks/:id', async (req, res) => {
-    const task = req.body;
+app.put('/api/tasks/:id',
+    [
+        check("id").isInt({ min: 1, max: 99 }),
+        check("description").isLength({ max: 40 }),
+        check("urgent").isBoolean(),
+        check("completed").isInt({ min: 0, max: 1 }),
+        check("private").isBoolean(),
+        check("deadline").isDate({ format: "YYYY-MM-DD", strictMode: true }),
+    ],
+    async (req, res) => {
+        const task = req.body;
 
-    try {
-        await dao.updateTask(task);
-        res.status(200).end();
-    }
-    catch (err) {
-        res.status(503).json({ error: `Database error while updating ${req.params.id}` });
-    }
-})
+        try {
+            await dao.updateTask(task);
+            res.status(200).end();
+        }
+        catch (err) {
+            res.status(503).json({ error: `Database error while updating ${req.params.id}` });
+        }
+    })
 
 // Mark task as completed/uncompleted
-app.put('/api/tasks/:id/:mark', async (req, res) => {
-    const task = await dao.getTask(req.params.id);
-    if (task.error)
-        res.status(404).json({ error: `Task with id=${req.params.id} not found!` });
-    try {
-        await dao.markTask(req.params.id, req.params.mark);
-        res.status(200).end();
-    }
-    catch (err) {
-        res.status(503).json({ error: `Error while marking task ${req.params.id}` });
-    }
-})
+app.put('/api/tasks/:id/:mark',
+    [
+        check("id").isInt({ min: 1, max: 99 }),
+        check("description").isLength({ max: 40 }),
+        check("urgent").isBoolean(),
+        check("completed").isInt({ min: 0, max: 1 }),
+        check("private").isBoolean(),
+        check("deadline").isDate({ format: "YYYY-MM-DD", strictMode: true }),
+    ], 
+    async (req, res) => {
+        const task = await dao.getTask(req.params.id);
+        if (task.error)
+            res.status(404).json({ error: `Task with id=${req.params.id} not found!` });
+        try {
+            await dao.markTask(req.params.id, req.params.mark);
+            res.status(200).end();
+        }
+        catch (err) {
+            res.status(503).json({ error: `Error while marking task ${req.params.id}` });
+        }
+    })
 
 app.delete('/api/tasks/:id', async (req, res) => {
     try {
